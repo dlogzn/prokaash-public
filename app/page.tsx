2402b357cc296c2b6feade1da09266d9a0b77664
dlogzn <dlogzn@gmail.com>
2024-11-10 04:18:18 +0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,54 @@
 import Image from "next/image";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Home | Prokaash",
+	description: "Cross Border E-Commerce Solutions",
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	return (
+		<section className="bg-white dark:bg-gray-900">
+				<div className="grid max-w-screen-xl px-4 py-4 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+						<div className="mr-auto place-self-center lg:col-span-7">
+								<div className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Promote Your Business Worldwide</div>
+								<div className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From checkout to global sales tax compliance, companies around the world use Prokaash to simplify their payment stack.</div>
+								<a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-sky-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+										Get started
+										<svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+								</a>
+								<a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+										Speak to Sales
+								</a>
+						</div>
+						<div className="lg:mt-0 lg:col-span-5 lg:flex">
+								<img src="../images/hero.png" alt="Hero" />
+						</div>
+				</div>
+				<div className="py-4 max-w-screen-xl mx-auto">
+					<p>PROKAASH™ is a global cross border e-Commerce firm that partners with Australia, Albania, Bangladesh, India, China, Canada, Emirates, Kazakhstan, Malaysia, Poland, Rwanda, Senegal, South Korea, Uzbekistan and Vietnam to tackle the most important challenges and capture the great opportunities for sellers to expand their market reach and increase sales volume around the globe.</p>
+					<h4>Cross Border e-Commerce:</h4>
+					<p>International sales in ebusiness We work according to the &quot;you sell with us abroad as well as in Poland&quot;. You don&#39;t have to know the specifics of the foreign market, the secrets of cross-border e-commerce and the habits of local consumers - that&#39;s what we do.</p>
+					<h4>Cross-Border Logistics:</h4>
+					<div>The development of your e-store begins abroad It&#39;s simple: no matter what you sell, more of your potential customers definitely live abroad than in the country. That is why cross-border sales are a natural and necessary step if you take your e-business seriously and think about its development.</div>
+					<h4>Logistics Service:</h4>
+					<div>We like to be where a lot is going on. We operate in countries where the e-Commerce market is developing particularly dynamically. We do Comprehensive logistics service for online stores.</div>
+					<ul>
+						<li>Give your Customers a Choice.</li>
+						<li>Home Delivery</li>
+						<li>Pick-up Points</li>
+						<li>Packet Vending Machines</li>
+						<li>Traditional Postal Operators</li>
+					</ul>
+					<p>We offer is complete cross-border logistics specially tailored to needs of online stores. This is not only the fastest delivery, but also multi-channel delivery:</p>
+					<ul>
+						<li>To Home</li>
+						<li>Pickup Points</li>
+						<li>Parcel Mac</li>
+					</ul>
+					<p>You sign only one contract, you use one API, one system and you get one invoice. In return, you have access to several markets, different types of forwarding and numerous additional services, such as:</p>
+				</div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+		</section>
+	);
 }
